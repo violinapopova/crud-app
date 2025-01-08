5e904244245d36101b6feea403b15cefbb965100
v.popova <violipopova@gmail.com>
2025-01-08 21:19:24 +0200

    created the ThemeContext

---
+++ constants/Colors.ts
@@ -0,0 +1,14 @@
+export const Colors = {
+  light: {
+    text: '#000',
+    background: '#fff',
+    icon: '#000',
+    button: 'royalblue',
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    icon: 'red',
+    button: '#000',
+  },
+};
---
+++ context/ThemeContext.js
@@ -0,0 +1,19 @@
+import { createContext, useState } from 'react';
+import { Appearance } from 'react-native';
+import { Colors } from '../constants/Colors';
+
+export const ThemeContext = createContext({
+    theme: Appearance.getColorScheme(),
+    toggleTheme: () => { },
+});
+
+export const ThemeProvider = ({ children }) => {
+    const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme());
+    const theme  = colorScheme === 'dark' ? Colors.dark : Colors.light;
+
+    return (
+        <ThemeContext.Provider value={{ colorScheme, setColorScheme, theme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+}
